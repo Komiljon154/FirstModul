7272cb5c05c268a19c94d14114f64ec3ac2fa87d
Komiljon154 <komiljonk154@gmail.com>
2024-11-23 07:22:42 +0500

    Ready

---
+++ 8-dars/ConsoleApp1/Models/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Car
+{
+    public Guid CarId { get; set; }
+
+    public string CarType { get; set; }
+
+    public string CarName { get; set; }
+
+    public string CarColor { get; set; }
+
+    public int CarPrice { get; set; }
+
+
+}
---
+++ 8-dars/ConsoleApp1/Models/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+internal class Student
+{
+    public Guid StudentId { get; set; }
+
+    public string StudentName { get; set; }
+
+    public int StudentAge { get; set; }
+
+    public string StudentPhoneNumber { get; set; }
+
+}
